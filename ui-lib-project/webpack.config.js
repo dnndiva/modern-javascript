f0942218ed94bbc1383d730adbddd4a3dea33b1d
Alessandra Daniels | DNN Diva <dnndiva@gmail.com>
2024-01-07 18:35:07 -0500

    upd

--- ui-lib-project/webpack.config.js
+++ ui-lib-project/webpack.config.js
@@ -1,31 +1,34 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist/assets'),
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    publicPath: '/assets/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  }
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist/assets/'),
+        filename: './bundle.js'
+    },
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        devMiddleware: {
+          publicPath: 'http://localhost:8080/assets/'
+        },
+        port: 8080,
+        hot: "only"
+    },
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: { presets: ["@babel/env"] }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: { extensions: ['*', '.js', '.jsx'] },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
 };
